3ed28e23e76a7060250432c553d2c7a031b45e83
koehr <n@koehr.in>
2019-06-25 23:03:06 +0200

    adds support for hours to date formatter

--- src/lib/date.js
+++ src/lib/date.js
@@ -5,9 +5,11 @@ export function dateFmt (date, formatString) {
   const year = date.getFullYear()
   const month = (`0${date.getMonth()}`).slice(-2)
   const day = (`0${date.getDate()}`).slice(-2)
+  const hour = (`0${date.getHours()}`).slice(-2)
 
   return formatString
          .replace('YYYY', year)
          .replace('MM', month)
          .replace('DD', day)
+         .replace('HH', hour)
 }
